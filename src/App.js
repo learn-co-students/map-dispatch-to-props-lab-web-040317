6eb2429554cda1910ba61a012a798a5b41290e71
aisleypay <paisley.singh@gmail.com>
2017-06-24 19:48:52 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ConnectedRestaurantInput } from './components/RestaurantInput';
 import { ConnectedRestaurants } from './components/Restaurants';
 
 export class App extends Component {
+
   render() {
     return (
       <div className="App">
